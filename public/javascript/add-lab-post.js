266cec729f48798ac02dd2fc06c6b72024a93515
Emily Hill <emhillmed2021@gmail.com>
2022-11-14 19:42:45 -0600

    added logic for add-lab-post.js

---
+++ public/javascript/add-lab-post.js
@@ -0,0 +1,34 @@
+async function newLabHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="notebook-title"]').value;
+    const purpose = document.querySelector('input[name="lab-purpose"]').value;
+    const background = document.querySelector('input[name="lab-background"]').value; 
+    const protocols = document.querySelector('input[name="lab-protocols"]').value; 
+    const observations = document.querySelector('input[name="lab-observations"]').value; 
+    const analysis = document.querySelector('input[name="lab-analysis"]').value; 
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        purpose,
+        background,
+        protocols,
+        observations,
+        analysis
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-lab-post').addEventListener('submit', newLabHandler);
+  
---
+++ public/javascript/add-notebook.js
@@ -0,0 +1,24 @@
+async function newNotebookHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="notebook-title"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  
