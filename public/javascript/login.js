3e04929f04e2ad0ad35d2ca73b6c8331a9b19a47
Emily Hill <emhillmed2021@gmail.com>
2022-11-17 22:14:50 -0600

    started tailwindcss, but might not count

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,10 +1,10 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
