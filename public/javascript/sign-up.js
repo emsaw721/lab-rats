2c28092d61f24ace64ce39cc555b5c4e254ad7ca
Emily Hill <emhillmed2021@gmail.com>
2022-11-22 12:54:17 -0600

    getting cannot get/project error

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -18,7 +18,7 @@
       });
   
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
