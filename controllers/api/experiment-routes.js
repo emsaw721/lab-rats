6934d6f56311f6aaa74478e4d3b7af189b68efe5
Emily Hill <emhillmed2021@gmail.com>
2022-11-22 16:02:52 -0600

    trying to find a way to link to experiment api from dashboard to view experiment list for each project

--- controllers/api/experiment-routes.js
+++ controllers/api/experiment-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../../config/connection');
 const { Experiment, Project, Comment, User } = require('../../models')
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
     console.log('======================');
     Experiment.findAll({
         attributes: [
