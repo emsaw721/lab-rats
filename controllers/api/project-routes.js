1f77fb5d787a948c5f6b2eb8bee6f5cebd52c3f2
TB <tatianabertazoli@gmail.com>
2022-11-21 22:37:12 -0600

    added post route for project

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Experiment, Project, User } = require('../../models')
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/',  (req, res) => {
     console.log('======================');
     Project.findAll({
         attributes: [
@@ -25,5 +25,43 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/', withAuth, (req, res) => {  
+    Project.create({
+      project_name: req.body.title
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+router.put('/:id', withAuth, (req, res) => {
+    Project.update(
+        {
+        attributes: [
+            'id',
+            'project_name',
+            'created_at'
+        ],
+    })
+      .then(dbPostData => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+          
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
 
 module.exports = router;
