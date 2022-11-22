84b142bdac974d656ff0dc3ed653fb22b1fb79d9
Emily Hill <emhillmed2021@gmail.com>
2022-11-22 17:11:50 -0600

    added new lab entry to edit-project.handlebars because I'm running out of time to figure out how to link those pages

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -35,21 +35,32 @@ router.get('/edit/:id', withAuth, (req, res) => {
         'id',
         'project_name',
         'created_at'
-    ],
-    // include: [
-    //   {
-    //     model: Comment,
-    //     attributes: ['id', 'comment_text', 'experiment_id', 'user_id'],
-    //     include: {
-    //       model: User,
-    //       attributes: ['username']
-    //     }
-    //   },
-    //   {
-    //     model: User,
-    //     attributes: ['username']
-    //   }
-    // ]
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-project', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:newlab', withAuth, (req, res) => {
+  Project.findByPk(req.params.id, {
+    attributes: [
+        'id',
+        'project_name',
+        'created_at'
+    ]
   })
     .then(dbPostData => {
       if (dbPostData) {
