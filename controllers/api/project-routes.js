102aa05c1689e3d470c1cd630a5c8f076385e3a7
TB <tatianabertazoli@gmail.com>
2022-11-22 19:34:19 -0600

    fixed post route for project

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Experiment, Project, User } = require('../../models')
+const { Project} = require('../../models')
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
@@ -12,18 +12,11 @@ router.get('/', (req, res) => {
             'created_at'
         ]
     })
-        .then(dbPostData => {
-            const projects = dbPostData.map(post => post.get({ plain: true }));
-            console.log(projects);
-            res.render('project', {
-                projects
-                // loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', withAuth, (req, res) => {
