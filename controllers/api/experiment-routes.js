35ccc037e313195e7e8c3f076aa297057278bfa8
TB <tatianabertazoli@gmail.com>
2022-11-23 12:07:34 -0600

    linked experiment to product id

--- controllers/api/experiment-routes.js
+++ controllers/api/experiment-routes.js
@@ -1,18 +1,21 @@
-const router = require('express').Router();
+const router = require('express').Router({ mergeParams: true });
 const sequelize = require('../../config/connection');
 const { Experiment, Project, Comment, User } = require('../../models')
 const withAuth = require('../../utils/auth');
 
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
   console.log('==========Experiments============');
-  Experiment.findByPk(req.params.id, {
+  Experiment.findAll({
+    where: {
+      project_id: req.params.project_id
+    },
     attributes: [
       'id',
       'title',
       'background',
       'protocols_calculations_reagents_equipment',
       'observations',
-      'analysis',      
+      'analysis',
       'project_id',
       'user_id',
       'created_at',
@@ -28,11 +31,47 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-  .then(dbPostData => res.json(dbPostData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  console.log('==========Experiments============');
+  Experiment.findOne({
+    where: {
+      project_id: req.params.project_id,
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'background',
+      'protocols_calculations_reagents_equipment',
+      'observations',
+      'analysis',
+      'project_id',
+      'user_id',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'user_id', 'experiment_id'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 //     .then(dbPostData => {
@@ -56,7 +95,12 @@ router.get('/:id', (req, res) => {
 router.post('/', withAuth, (req, res) => {
   Experiment.create({
     title: req.body.title,
-    description: req.body.description
+    purpose_and_hypothesis: req.body.purpose_and_hypothesis,
+    background: req.body.background,
+    protocols_calculations_reagents_equipment: req.body.protocols_calculations_reagents_equipment,
+    observations: req.body.observations,
+    analysis:req.body.analysis,
+    project_id: req.params.project_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 
+
 const userRoutes = require("./user-routes.js");
 const experimentRoutes = require('./experiment-routes');
 const commentRoutes = require("./comment-routes");
 const projectRoutes = require('./project-routes');
 
 router.use('/users', userRoutes);
-router.use('/experiments', experimentRoutes);
+router.use('/projects/:project_id/experiments', experimentRoutes);
 router.use("/comments", commentRoutes);
 router.use('/projects', projectRoutes);
+
+
+
+
 
 module.exports = router;
