78443fa06544c78abd3af6a5297e5f4c7102ef0d
TB <tatianabertazoli@gmail.com>
2022-11-22 20:07:34 -0600

    fixed update project title

--- public/javascript/edit-project.js
+++ public/javascript/edit-project.js
@@ -1,24 +1,27 @@
 
 async function editFormHandler(event) {
-   
-    const title = document.querySelector('input[name="project-title"]').value.trim();
-    const id = document.querySelector('#notebook-id').getAttribute('data-id');
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
 
-  document.querySelector('.edit-project-form').addEventListener('submit', editFormHandler);
-  
+  const title = document.querySelector('input[name="project-title"]').value.trim();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/projects/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.edit-project-form').addEventListener('submit', editFormHandler);
