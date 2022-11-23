cdaa8181f24b6bc84e8763b0a4083e75de3508dd
TB <tatianabertazoli@gmail.com>
2022-11-23 12:13:37 -0600

    created post and delete experiment routes

--- controllers/api/experiment-routes.js
+++ controllers/api/experiment-routes.js
@@ -109,4 +109,26 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
+  Experiment.destroy({
+      where: {
+        project_id: req.params.project_id,
+        id: req.params.id
+      }
+  })
+      .then(dbPostData => {
+          if (!dbPostData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(dbPostData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+
 module.exports = router;
