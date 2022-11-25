7576b932af827eb4ab0690378e203db3bd0ce38d
TB <tatianabertazoli@gmail.com>
2022-11-25 13:34:27 -0600

    working on edit routes

--- public/javascript/edit-lab.js
+++ public/javascript/edit-lab.js
@@ -1,7 +1,11 @@
 
 // go to edit-lab.handlebars for issues 
 async function editLabHandler(event) {
+<<<<<<< HEAD
    
+=======
+    event.preventDefault();
+>>>>>>> 84c2141 (working on edit routes)
 
     const title = document.querySelector('input[name="lab-title"]').value.trim();
     const purpose = document.querySelector('#lab-purpose').value.trim();
@@ -10,9 +14,16 @@ async function editLabHandler(event) {
     const observations = document.querySelector('#lab-obs').value.trim();
     const analysis = document.querySelector('#lab-analysis').value.trim();
 
+<<<<<<< HEAD
   
 
     const response = await fetch(`/api/projects/{{experiment.project_id}}/experiments/${id}`, {
+=======
+    const projectid = window.location.href
+    console.log(projectid)
+
+    const response = await fetch(`projectid`, {
+>>>>>>> 84c2141 (working on edit routes)
         method: 'PUT',
         body: JSON.stringify({
             title,
@@ -26,10 +37,18 @@ async function editLabHandler(event) {
     });
 
     if (response.ok) {
+<<<<<<< HEAD
         document.location.replace('/experiment-list')
+=======
+        document.location.replace('/notebook')
+>>>>>>> 84c2141 (working on edit routes)
     } else {
         alert(response.statusText);
     }
 };
 
+<<<<<<< HEAD
 document.querySelector('.save-lab-btn').addEventListener('submit', editLabHandler); 
+=======
+document.querySelector('#edit-lab-form').addEventListener('submit', editLabHandler); 
+>>>>>>> 84c2141 (working on edit routes)
