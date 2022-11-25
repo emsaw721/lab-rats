923526a684dea16f213e30fa73712199102e1dac
Emily Hill <emhillmed2021@gmail.com>
2022-11-25 13:55:37 -0600

    went back to original view-experiments.js that I accidentally changed on develop

--- public/javascript/view-experiments.js
+++ public/javascript/view-experiments.js
@@ -1,41 +1,6 @@
 
 
 
-async function viewLabHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('#experiment-lab-link');
-  
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-  
-    const response = await fetch(`/api/projects/${id}/experiments`, {
-      method: 'GET',
-      body: JSON.stringify({
-        title,
-        purpose_and_hypothesis,
-        background,
-        protocols_calculations_reagents_equipment,
-        observations, 
-        analysis,
-        project_id,
-        user_id,
-        created_at
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-  
-
 async function viewLabsHandler(event) {
   event.preventDefault();
 
