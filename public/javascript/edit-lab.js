d9bb61949ab68d5cec3fc8f16fe1458531a54d12
Emily Hill <emhillmed2021@gmail.com>
2022-11-25 18:39:50 -0600

    can't figure out how to make the edit and delete routes work

--- public/javascript/edit-lab.js
+++ public/javascript/edit-lab.js
@@ -1,11 +1,6 @@
 
 // go to edit-lab.handlebars for issues 
 async function editLabHandler(event) {
-<<<<<<< HEAD
-   
-=======
-    event.preventDefault();
->>>>>>> 84c2141 (working on edit routes)
 
     const title = document.querySelector('input[name="lab-title"]').value.trim();
     const purpose = document.querySelector('#lab-purpose').value.trim();
@@ -14,16 +9,14 @@ async function editLabHandler(event) {
     const observations = document.querySelector('#lab-obs').value.trim();
     const analysis = document.querySelector('#lab-analysis').value.trim();
 
-<<<<<<< HEAD
-  
+    experimentid = document.querySelector('#lab-id').getAttribute('data-id'); 
+    console.log(experimentid)
 
-    const response = await fetch(`/api/projects/{{experiment.project_id}}/experiments/${id}`, {
-=======
-    const projectid = window.location.href
+
+    projectid = document.querySelector('#project-id').getAttribute('data-id'); 
     console.log(projectid)
 
-    const response = await fetch(`projectid`, {
->>>>>>> 84c2141 (working on edit routes)
+    const response = await fetch(`/api/projects/${projectid}/experiments/${experimentid}`, {
         method: 'PUT',
         body: JSON.stringify({
             title,
@@ -37,18 +30,13 @@ async function editLabHandler(event) {
     });
 
     if (response.ok) {
-<<<<<<< HEAD
+
         document.location.replace('/experiment-list')
-=======
-        document.location.replace('/notebook')
->>>>>>> 84c2141 (working on edit routes)
+
     } else {
         alert(response.statusText);
     }
 };
 
-<<<<<<< HEAD
 document.querySelector('.save-lab-btn').addEventListener('submit', editLabHandler); 
-=======
-document.querySelector('#edit-lab-form').addEventListener('submit', editLabHandler); 
->>>>>>> 84c2141 (working on edit routes)
+
